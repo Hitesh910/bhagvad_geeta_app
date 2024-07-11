325c6cee5b0d0c5bfe161d12b64619e23172d9b9
Hitesh <Hiteshkhatri910@gmail.com>
2024-07-11 17:08:39 +0530

    BhagvadGeetaApp

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:bhagvad_geeta_app/utils/app_routes.dart';
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(MaterialApp(routes: app_routes,));
+}
---
+++ lib/utils/app_routes.dart
@@ -0,0 +1,7 @@
+
+import 'package:bhagvad_geeta_app/screen/home/view/home_screen.dart';
+import 'package:flutter/material.dart';
+
+Map<String,WidgetBuilder> app_routes = {
+  "/":(context) => HomeScreen(),
+};
